610c8efb5f85d40f98ec6d4df5dfdcf85e2a14e3
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2022-03-09 07:15:00 +0300

    change testcase assert

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+print(np.dot(np.array([4]), np.exp(3.1)))
+print(4 * np.exp(3.1))
+print(np.exp(3.1,))
+
+
+print( (np.dot(np.array([4.0]), np.exp(3.1)) ))
+print( tuple [ np.dot(np.array([4]), np.exp(3.1))])
+print( np.exp( np.array(3.1, np.float32)))
+x=3.1
+print(type(x))
+
+print(np.dot(4, np.exp(np.array(3.1, np.float32))))
+print((np.maximum(1,2)))
