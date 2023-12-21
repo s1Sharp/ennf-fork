6239f77f17bddbc87b9bd0e744384c80d232e7cf
Timur Bikbulatov <ch7cly@gmail.com>
2023-12-21 21:10:59 +0300

    loss plotter added, kek

---
+++ toy_mlp/history_plotter.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+def plot_loss(losses = list[list], loss_names: list[str] = ['train loss'],tittle:str='Loss over epoches'):
+  plt.figure(figsize=(15, 9))
+  for loss, ttl in zip(losses,loss_names):
+    plt.plot(loss, label=ttl)
+
+  plt.legend(loc='best')
+  plt.xlabel("epochs")
+  plt.ylabel("loss")
+  plt.title(tittle)
+  plt.show()
